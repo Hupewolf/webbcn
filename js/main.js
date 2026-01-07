c3253037028a54d1e8f3a4df5e3958e5d6b3d63c
Huy <Huyanh052222@gmail.com>
2026-01-07 11:10:27 +0700

    Add hyperlink on index, creat main board on userPage

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+// Function for Header (Dung co xoa no)
+function noticBar(){
+    // alert("Hola!");
+    const notiBar = document.getElementById("notification");
+    if (notiBar.style.display === 'flex') {
+        notiBar.classList.toggle("is-visible");
+    } else {
+        notiBar.style.display = 'none';
+        notiBar.style.display = 'flex';
+    }
+}
