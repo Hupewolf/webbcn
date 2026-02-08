5fd0f007373be9fe5a211fb30a5b53e1c8bd2fe5
Hupewolf <nhannguyen.ix5@gmail.com>
2026-02-08 18:41:43 +0700

    feature/addfilternhan: chưa fixbug được

--- js/userPage.js
+++ js/userPage.js
@@ -18,3 +18,72 @@ document.getElementById("goContact").addEventListener("click", () => {
         behavior: "smooth" 
     });
 });
+
+// const chips = document.querySelectorAll('.chip');
+// const cards = document.querySelectorAll('.project-card');
+// const searchInput = document.getElementById('searchInput');
+
+// const drawer = document.getElementById('drawer');
+// const overlay = document.getElementById('overlay');
+// const openFilter = document.getElementById('openFilter');
+// const apply = document.getElementById('apply');
+// const reset = document.getElementById('reset');
+
+// let activeType = 'all';
+// let activeTags = [];
+
+// chips.forEach(chip => {
+//     chip.onclick = () => {
+//         chips.forEach(c => c.classList.remove('actives'));
+//         chip.classList.add('actives');
+//         activeType = chip.dataset.type;
+//         filter();
+//     };
+// });
+
+// searchInput.oninput = filter;
+
+// openFilter.onclick = () => {
+//     drawer.classList.add('show');
+//     overlay.classList.add('show');
+// };
+
+// overlay.onclick = closeDrawer;
+
+// function closeDrawer() {
+//     drawer.classList.remove('show');
+//     overlay.classList.remove('show');
+// }
+
+// apply.onclick = () => {
+//     activeTags = [...drawer.querySelectorAll('input:checked')]
+//         .map(i => i.value);
+//     closeDrawer();
+//     filter();
+// };
+
+// reset.onclick = () => {
+//     drawer.querySelectorAll('input').forEach(i => i.checked = false);
+//     activeTags = [];
+//     filter();
+// };
+
+// function filter() {
+//     const keyword = searchInput.value.toLowerCase();
+
+//     cards.forEach(card => {
+//         const type = card.dataset.type;
+//         const tags = card.dataset.tags;
+//         const text = card.innerText.toLowerCase();
+
+//         const matchType = activeType === 'all' || type === activeType;
+//         const matchSearch = text.includes(keyword);
+//         const matchTag = activeTags.length === 0 ||
+//             activeTags.some(tag => tags.includes(tag));
+
+//         card.classList.toggle(
+//             'hide',
+//             !(matchType && matchSearch && matchTag)
+//         );
+//     });
+// }
